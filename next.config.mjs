3c429834c8b898228e7154d2c439ddb22a1a3664
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-09-02 16:29:23 -0300

    chore: adding more remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'lovely-flamingo-139.convex.cloud'
-        }]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "artful-turtle-607.convex.cloud",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
